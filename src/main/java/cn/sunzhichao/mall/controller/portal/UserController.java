fc249f7b4d0247523585cf575e6d18df9a35ac1b
Sun ZhiChao <sunzhichao@sunzhichao.net>
2021-07-10 21:25:36 +0800

    用户登出功能

--- src/main/java/cn/sunzhichao/mall/controller/portal/UserController.java
+++ src/main/java/cn/sunzhichao/mall/controller/portal/UserController.java
@@ -37,4 +37,12 @@ public class UserController {
         }
         return response;
     }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
 }
