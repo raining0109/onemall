b10f9d6e7e634f6708834075c6cd9cc48f54be86
Sun ZhiChao <sunzhichao@sunzhichao.net>
2021-07-13 15:59:45 +0800

    更新品类名字功能

--- src/main/java/cn/sunzhichao/mall/controller/backend/CategoryManageController.java
+++ src/main/java/cn/sunzhichao/mall/controller/backend/CategoryManageController.java
@@ -46,4 +46,21 @@ public class CategoryManageController {
             return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
     }
+
+    @RequestMapping(value = "set_category_name.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session, Integer categoryId, String categoryName) {
+        //判断是否登录
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录");
+        }
+        //校验一下是否是管理员
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //业务逻辑，更新categoryName
+            return iCategoryService.updateCategoryName(categoryId, categoryName);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
 }
--- src/main/java/cn/sunzhichao/mall/service/ICategoryService.java
+++ src/main/java/cn/sunzhichao/mall/service/ICategoryService.java
@@ -5,4 +5,6 @@ import cn.sunzhichao.mall.common.ServerResponse;
 public interface ICategoryService {
 
     ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
 }
